1a40751160273735b670ec6dc5c16a6fe5cc652a
root <odunsisamuel876@gmail.com>
2022-07-05 04:38:34 +0000

    My commit

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
+    n = len(my_list)
     if idx < 0:
-        return (None)
-
-    length = len(my_list)
-
-    if idx > length - 1:
-        return (None)
-
-    return(my_list[idx])
+        return None
+    elif idx > n - 1:
+        return None
+    else:
+        return my_list[idx]
