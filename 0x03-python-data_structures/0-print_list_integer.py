d45c7f6a5cd86d7c1c764868d0fdc89a8dfc8f73
root <odunsisamuel876@gmail.com>
2022-07-05 23:25:25 +0000

    My commit

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-def print_list_integer(my_lists=[]):
-	for j in range(len(my_list)):
-		print("{:i}".format(my_list[j]))
+def print_list_integer(my_list=[]):
+    for i in range(len(my_list)):
+        print("{:d}".format(my_list[i]))
