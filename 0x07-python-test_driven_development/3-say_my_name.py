e9d068ffd2047669bf6f6e4027db67ff6db98c20
root <odunsisamuel876@gmail.com>
2022-07-30 04:34:40 +0000

    My commit

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,19 +1,17 @@
 #!/usr/bin/python3
 """
-Module say_my_name
-Prints a given first name and last name.
+    No module allowed to be imported
 """
 
 
 def say_my_name(first_name, last_name=""):
-    """Prints a string with <first_name>
-    and <last_name>.
     """
-
-    if type(first_name) is not str:
+        prints my name
+    """
+    if first_name == "" and last_name == "":
+        return None
+    if type(first_name) != str:
         raise TypeError("first_name must be a string")
-
-    if type(last_name) is not str:
+    if type(last_name) != str:
         raise TypeError("last_name must be a string")
-
     print("My name is {} {}".format(first_name, last_name))
