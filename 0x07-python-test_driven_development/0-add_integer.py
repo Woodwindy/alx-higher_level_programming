31a8ad5d8852f3d7ba910accecc71fb7ebc8af5a
root <odunsisamuel876@gmail.com>
2022-07-30 01:03:01 +0000

    My commit

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,23 +1,22 @@
 #!/usr/bin/python3
 """
-A program to  add two integers
+Adds two integer together
 """
 
 
 def add_integer(a, b=98):
+    """Returns the addition of a and b,
+    or an error if a and b are not integers or floats
     """
-        Adds two integers
-    """
-    try:
-        assert type(a) in (float, int)
-    except:
+
+    if type(a) is not int and type(a) is not float:
         raise TypeError("a must be an integer")
-    try:
-        assert type(b) in (float, int)
-    except:
+    if type(b) is not int and type(b) is not float:
         raise TypeError("b must be an integer")
-    if type(a) == float:
+
+    if type(a) is float:
         a = int(a)
-    if type(b) == float:
+    if type(b) is float:
         b = int(b)
+
     return a + b
