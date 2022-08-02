048ef2a6d7c7cc3af6a4472c9c898a993b799681
root <odunsisamuel876@gmail.com>
2022-08-02 01:32:31 +0000

    A function that inherits from list

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""A function that iherits from list"""
+
+
+class MyList(list):
+    """inherits from list"""
+
+    def print_sorted(self):
+        """print sorted list"""
+        print(sorted(self))
