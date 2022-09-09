13584bc45d620ee6ddb328b7c8157d5c2ddcb264
Woodwindy <odunsisamuel876@gmail.com>
2022-09-08 21:37:49 -0700

    delete hbtn_0c_0 database

--- 0x0D-SQL_introduction/2-remove_database.sql
+++ 0x0D-SQL_introduction/2-remove_database.sql
@@ -1,3 +1,3 @@
 -- script that deletes the database hbtn_0c_0
 -- the script will run if the database does not exist initially
-DROP DATABASE IF NOT  EXISTS hbtn_0c_0;
+DROP DATABASE IF NOT  EXISTS hbtn_0c_0
