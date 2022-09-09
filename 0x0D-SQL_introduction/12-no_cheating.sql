5426e596ee5c8bd09b58f879d35c05b59822ee64
Woodwindy <odunsisamuel876@gmail.com>
2022-09-09 13:59:15 -0700

    Update second_table

---
+++ 0x0D-SQL_introduction/12-no_cheating.sql
@@ -0,0 +1,3 @@
+-- a script that updates the score of Bob to 10 in the table second_table.
+-- You are not allowed to use Bob’s id value, only the name field.
+SELECT score FROM second_table UPDATE score VALUE (10)
