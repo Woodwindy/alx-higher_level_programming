56d774d395dec85387725638206dfb76b8633b51
root <odunsisamuel876@gmail.com>
2022-10-11 21:27:25 +0000

    JS say something sweet about C, Python and yourself

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+/* a script that prints C is fun, Python is cool
+and JavaScript is amazing */
+console.log('C is fun');
+console.log('Python is cool');
+console.log('JavaScript is amazing');
