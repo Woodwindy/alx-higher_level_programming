2b7f0fa29d24066da6865c592eaedc804b34cfd4
root <odunsisamuel876@gmail.com>
2022-10-13 20:57:21 +0000

    Love for C: reoccurrence of C

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+const myArgs = process.argv[2]
+let i = 0;
+if (isNaN(myArgs{[0])) {
+  console.log('Missing number of occurrences');
+} else {
+  while (i = 0, i < myArgs, i++);
+    console.log('C is fun');
+}
